debd6c2383e63264e3080ef625ef0e6fbe6bd3f4
Nattaphong Bupasiri <nat@ideabranch.co.th>
2017-08-12 15:30:07 +0700

    Test Reply

--- bot.php
+++ bot.php
@@ -10,9 +10,9 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'วงใน') {
 			// Get text sent
-			$text = $event['message']['text'];
+			$text = $event['message']['วงในเงียบ'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
