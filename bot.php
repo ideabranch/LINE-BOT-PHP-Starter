c55ba96020f11f5eed4cece76e713b91949887cd
Nattaphong Bupasiri <nat@ideabranch.co.th>
2017-08-12 17:46:35 +0700

    test

--- bot.php
+++ bot.php
@@ -82,7 +82,31 @@ if (!is_null($events['events'])) {
                     'text' => '...'
                 ];
                 
-             } 
+             }elseif ($text == 'คืนนี้ร้านไหนดี'){
+                
+                 $messages = [
+                    "type": "template",
+                    "altText": "this is a confirm template",
+                    "template": {
+                        "type": "confirm",
+                        "text": "Are you sure?",
+                        "actions": [
+                            {
+                              "type": "message",
+                              "label": "Yes",
+                              "text": "yes"
+                            },
+                            {
+                              "type": "message",
+                              "label": "No",
+                              "text": "no"
+                            }
+                        ]
+                    }
+                ];
+                
+             }  
+             
             //  else {
 
             //     $messages = [
