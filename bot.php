b01498888ea0496d3ac598a16a9cd3d701f8d5da
Nattaphong Bupasiri <nat@ideabranch.co.th>
2017-08-12 16:52:11 +0700

    test

--- bot.php
+++ bot.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
                     'text' => 'งานยังไม่เสร็จครับ! ขอเลื่อนเป็นพรุ่งนี้ได้ไหมครับ T^T'
                 ];
 
-            }elseif ($text == 'ideabranch'){
+            }elseif (strtolower($text) == 'ideabranch'){
 
                 $messages = [
                     'type' => 'text',
